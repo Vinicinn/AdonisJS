af38578e595e183aed1dcf01a7bed1bd3cc29392
vinicius <viniciusmelo554@gmail.com>
2022-10-28 19:20:40 -0300

    update

--- notas/app/Controllers/Http/NotasController.ts
+++ notas/app/Controllers/Http/NotasController.ts
@@ -2,31 +2,41 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Nota from 'App/Models/Nota'
 
 export default class NotasController {
+  
   public async index({}: HttpContextContract) {
     return Nota.all()
   }
-
+  
   public async store({ request }: HttpContextContract) {
-    let nota = request.body()
-    await Nota.create(nota)
-    return `Nota criada com sucesso!\n${JSON.stringify(nota)}`
+    let body = request.body()
+    let nota = await Nota.create(body)
+    return `Nota ${nota.titulo} criada com sucesso!`
   }
-
+  
   public async show({ params }: HttpContextContract) {
     let id = params.id
-    let row = await Nota.findBy("id",id)
-    let titulo = row?.$getAttribute("titulo")
-    let corpo = row?.$getAttribute("corpo")
-    return `Mostando nota ${id}:\nTitulo: ${titulo}\nCorpo: ${corpo}`
+    let nota = await Nota.findByOrFail('id',id)
+    return nota
   }
 
-  public async update({ params }: HttpContextContract) {
+  public async update({ params, request }: HttpContextContract) {
     let id = params.id
-    return `Nota ${id} atualizada`
+    let body = request.body()
+    let nota = await Nota.findByOrFail('id',id)
+    if (body.corpo != null) {
+      nota.corpo = body.corpo
+    }
+    if (body.titulo != null) {
+      nota.titulo = body.titulo
+    }
+    nota.save()
+    return nota
   }
 
-  public async destroy({  }: HttpContextContract) {
-    Nota.truncate()
-    return `Tabela removida`
+  public async destroy({ params }: HttpContextContract) {
+    let id = params.id
+    let nota = await Nota.findByOrFail('id',id)
+    nota.delete()
+    return `nota ${nota} removida`
   }
 }
